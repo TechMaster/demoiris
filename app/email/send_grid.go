7e8ca8db5312ae36c254d8ba8f420a22243a2cac
techmaster <cuong@techmaster.vn>
2021-08-11 08:14:03 +0700

    remove sendgrid key

--- app/email/send_grid.go
+++ app/email/send_grid.go
@@ -14,7 +14,7 @@ func (sg SendGrid) SendSimple(to string, subject string, body string) error {
 	plainTextContent := "and easy to do anywhere, even with Go"
 	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
 	message := mail.NewSingleEmail(from, subject, receipient, plainTextContent, htmlContent)
-	client := sendgrid.NewSendClient("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")
+	client := sendgrid.NewSendClient("")
 	if _, err := client.Send(message); err != nil {
 		return err
 	} else {
